42cf7cb64f694b5eb86ba3a2a7a5a92743135999
kyohei-917 <haru.kk.917@gmail.com>
2021-02-03 16:04:34 +0900

    Add: slim化

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module MeatHead
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # デフォルトをslimに変更
+    config.generators.template_engine = :slim
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
