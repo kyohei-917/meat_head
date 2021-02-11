cd463d36619f27a13d07308e44d9bf5329e553ae
kyohei-917 <haru.kk.917@gmail.com>
2021-02-11 09:09:18 +0900

    Fix: ゲストユーザー削除防止メソッド

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root to: 'homes#index'
   get 'homes/index'
 
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
