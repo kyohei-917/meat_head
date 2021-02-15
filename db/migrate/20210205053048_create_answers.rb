6932f84624c53884ab8d114cbfda180b0ebbf8b2
kyohei-917 <haru.kk.917@gmail.com>
2021-02-16 07:18:28 +0900

    Fix: 修正後のER図に沿ってmigrationファイルをリセット、再作成

--- db/migrate/20210205053048_create_answers.rb
+++ db/migrate/20210205053048_create_answers.rb
@@ -1,7 +1,6 @@
 class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
-      t.integer :choice
       t.references :user, foreign_key: true
 
       t.timestamps
