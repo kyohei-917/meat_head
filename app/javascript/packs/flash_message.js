fc16e2fac8359f6bac8432d1bdcc1533e0fdae9a
kyohei-917 <haru.kk.917@gmail.com>
2021-03-05 14:33:05 +0900

    いくつかの軽微な修正

--- app/javascript/packs/flash_message.js
+++ app/javascript/packs/flash_message.js
@@ -1,5 +1,6 @@
-$(function () {
-  $(".closable .close.icon").on("click", () => {
-    $('.closable').fadeOut("slow");
+$(document).on('turbolinks:load', function() {
+  // フラッシュメッセージを3秒で消す
+  $(function(){
+    setTimeout("$('.flash-message-wrapper').fadeOut('slow')", 3000)
   });
 });
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,7 @@ Rails.application.configure do
 
   # devise setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # better_errors
+  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
 end
