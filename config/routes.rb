7e5e8b6de6483476322201c4df2616f997a6ea0b
kyohei-917 <haru.kk.917@gmail.com>
2021-03-05 14:29:03 +0900

    Add: 診断結果別ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/top'
 
+  # 診断結果別views
+  get 'answers/result100'
+  get 'answers/result70'
+  get 'answers/result50'
+  get 'answers/result30'
+  get 'answers/result0'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
@@ -10,7 +17,9 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
 
-  resources :users
+  resources :users do
+    resources :qiestions
+  end
   resources :questions
   resources :answers do
     resources :answer_details
